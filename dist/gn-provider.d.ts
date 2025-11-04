ae56e8bda1efb82e94e214804f444e6309658958
Boris Javier Barrera <borisjavier@gmail.com>
2025-11-04 00:46:12 -0300

    small modifications to final release.

---
+++ dist/gn-provider.d.ts
@@ -0,0 +1,36 @@
+import * as scryptlib from 'scryptlib';
+import { Provider, TransactionResponse, TxHash, UtxoQueryOptions } from 'scrypt-ts/dist/bsv/abstract-provider';
+import { AddressOption, UTXO } from 'scrypt-ts/dist/bsv/types';
+declare enum ProviderEvent {
+    Connected = "connected",
+    NetworkChange = "networkChange"
+}
+export type UTXOWithHeight = UTXO & {
+    height: number;
+};
+export declare class GNProvider extends Provider {
+    emit: (event: ProviderEvent, ...args: any[]) => boolean;
+    private _network;
+    private _isConnected;
+    private _apiKey;
+    private apiPrefix;
+    private _getHeaders;
+    connect: () => Promise<this>;
+    getFeePerKb: () => Promise<number>;
+    constructor(network: scryptlib.bsv.Networks.Network, apiKey?: string);
+    isConnected: () => boolean;
+    updateNetwork: (network: scryptlib.bsv.Networks.Network) => void;
+    getNetwork: () => scryptlib.bsv.Networks.Network;
+    protected _ready: () => Promise<void>;
+    sendRawTransaction: (rawTxHex: string) => Promise<TxHash>;
+    sendTransaction: (signedTx: scryptlib.bsv.Transaction) => Promise<string>;
+    listUnspent: (address: AddressOption, options?: UtxoQueryOptions) => Promise<UTXOWithHeight[]>;
+    getBalance: (address: AddressOption) => Promise<{
+        confirmed: number;
+        unconfirmed: number;
+    }>;
+    getTransaction: (txHash: string) => Promise<TransactionResponse>;
+    private needIgnoreError;
+    private friendlyBIP22RejectionMsg;
+}
+export {};
